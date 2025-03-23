c4a027cce5a0f9c504585a9b1f386f1b5c50b26a
HSZemi <hszemi@users.noreply.github.com>
2025-03-24 00:04:47 +0100

    Log bans in dedicated channels

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import os
 
 import discord
+from discord import Guild, Member, User
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,11 +24,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        return
     for bad_word in ('@everyone', '@here'):
         if bad_word in message.content:
             print(f'[{message.created_at}] Banning {message.author} for {message.content=} in {message.guild=}')
             await message.author.ban(reason=f'Tried to use {bad_word}', delete_message_seconds=120)
+            await send_log_message(message.author, message.content, message.guild)
             return
+
+async def send_log_message(author: User | Member, content: str, guild: Guild):
+    for channel in guild.text_channels:
+        if channel.name in ('actual-log', 'alerta'):
+            escaped_message = content.replace('```', '` ` `')
+            await channel.send(f'Banning <@{author.id}> for message:\n```\n{escaped_message}\n```')
+
 
 
 def main():
