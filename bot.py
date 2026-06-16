9e903e84bb16baaa57129686340f2751ee17e0af
HSZemi <hszemi@users.noreply.github.com>
2026-06-16 23:33:52 +0200

    Introduce new logic for warnings and bans

--- bot.py
+++ bot.py
@@ -2,19 +2,28 @@
 import asyncio
 import os
 from dataclasses import dataclass
+from enum import Enum, auto
+from io import BytesIO
 
 import discord
-from discord import Guild, Member, User
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-SUS_ITEMS_THRESHOLD = 4
-SUS_POSTS_THRESHOLD = 2
-LINK_THRESHOLD = 4
-CHANNEL_THRESHOLD = 5
 SECONDS_THRESHOLD = 30
+
+BAD_WORD_WARN_THRESHOLD_INCL = 1
+BAD_WORD_BAN_THRESHOLD_INCL = 2
+
+MSG_WARM_THRESHOLD_INCL = 3
+MSG_BAN_THRESHOLD_INCL = 4
+
+MEDIA_WARN_THRESHOLD_INCL = 2
+MEDIA_BAN_THRESHOLD_INCL = 3
+
+BAD_WORDS = ('@everyone', '@here')
 
 EXEMPT_CHANNEL_NAMES = [
     't90-elitechatroom',
@@ -39,12 +48,14 @@ class ChannelPost:
     channel_id: int
     author_id: int
     timestamp: float
-    sus: bool
-    links: int
+    media_tuple: tuple[int, int, int] | None
+    contains_bad_word: bool
 
 
-def is_suspicious(message: discord.Message) -> bool:
-    return len(message.attachments) + len(message.embeds) >= SUS_ITEMS_THRESHOLD
+class ModAction(Enum):
+    NOTHING = auto()
+    WARN = auto()
+    BAN = auto()
 
 
 def count_links(message: discord.Message) -> int:
@@ -58,36 +69,59 @@ def is_exempt(message: discord.Message) -> bool:
     return 'Administrator' in roles or 'Mod' in roles
 
 
+def to_media_tuple(message: discord.Message) -> tuple[int, int, int] | None:
+    media_tuple = (count_links(message), len(message.attachments), len(message.embeds))
+    return media_tuple if any(media_tuple) else None
+
+
 class RecentPosts:
     def __init__(self):
         self.recent_posts = []
         self.lock = asyncio.Lock()
 
-    async def should_ban(self, message: discord.Message) -> bool:
+    async def get_mod_action(self, message: discord.Message) -> tuple[ModAction, str]:
         async with self.lock:
             channel_id = message.channel.id
             author_id = message.author.id
             now = message.created_at.timestamp()
             limit = now - SECONDS_THRESHOLD
             self.recent_posts = [p for p in self.recent_posts if p.timestamp > limit]
-            sus = is_suspicious(message)
-            if sus and not is_exempt(message):
-                await message.reply('That message looked sus so I deleted it. Do not do it again or I will ban you.')
-                description = f'{len(message.attachments)} attachments and {len(message.embeds)} embeds'
-                await send_sus_delete_log_message(message.author, message.guild, description)
-                await message.delete()
-            links = count_links(message)
+            if is_exempt(message):
+                return ModAction.NOTHING, ""
+            media_tuple = to_media_tuple(message)
+            contains_bad_word = any(w in message.content for w in BAD_WORDS)
             self.recent_posts.append(ChannelPost(
-                channel_id=channel_id, author_id=author_id, timestamp=now, sus=sus, links=links
+                channel_id=channel_id,
+                author_id=author_id,
+                timestamp=now,
+                media_tuple=media_tuple,
+                contains_bad_word=contains_bad_word,
             ))
-            recent_channels_count = len({p.channel_id for p in self.recent_posts if p.author_id == author_id})
-            too_many_channels = recent_channels_count >= CHANNEL_THRESHOLD
-            too_sus = len([p for p in self.recent_posts if p.sus and p.author_id == author_id]) >= SUS_POSTS_THRESHOLD
-            too_many_links = sum([p.links for p in self.recent_posts if p.author_id == author_id]) >= LINK_THRESHOLD
-            return too_many_channels or too_sus or too_many_links
+            posts_by_author = [p for p in self.recent_posts if p.author_id == author_id]
+            bad_words_count = len({p for p in posts_by_author if p.contains_bad_word})
+            same_media_tuple_count = len(
+                [p for p in posts_by_author if p.media_tuple == media_tuple]) if media_tuple else 0
+            recent_channels_count = len({p.channel_id for p in posts_by_author})
+
+            if same_media_tuple_count >= MEDIA_BAN_THRESHOLD_INCL:
+                return ModAction.BAN, f"Same media types in {same_media_tuple_count}/{MEDIA_BAN_THRESHOLD_INCL} messages within {SECONDS_THRESHOLD} seconds"
+            if recent_channels_count >= MSG_BAN_THRESHOLD_INCL:
+                return ModAction.BAN, f"Posted in {recent_channels_count}/{MSG_BAN_THRESHOLD_INCL} channels within {SECONDS_THRESHOLD} seconds"
+            if bad_words_count >= BAD_WORD_BAN_THRESHOLD_INCL:
+                return ModAction.BAN, f"Posted {bad_words_count}/{BAD_WORD_BAN_THRESHOLD_INCL} bad words within {SECONDS_THRESHOLD} seconds"
+
+            if same_media_tuple_count >= MEDIA_WARN_THRESHOLD_INCL:
+                return ModAction.WARN, "Slow down with your posting or you will get banned"
+            if recent_channels_count >= MSG_WARM_THRESHOLD_INCL:
+                return ModAction.WARN, "Slow down with your posting or you will get banned"
+            if bad_words_count >= BAD_WORD_WARN_THRESHOLD_INCL:
+                return ModAction.WARN, "Do not try to tag this many people you silly goose. It does not work, and you will get banned if you try again."
+
+            return ModAction.NOTHING, ""
 
 
 RECENT_POSTS = RecentPosts()
+
 
 @client.event
 async def on_ready():
@@ -97,42 +131,39 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author.bot:
         return
-    for bad_word in ('@everyone', '@here'):
-        if bad_word in message.content:
-            print(f'[{message.created_at}] Banning {message.author} for {message.content=} in {message.guild=}')
-            await message.author.ban(reason=f'Tried to use {bad_word}', delete_message_seconds=120)
-            await send_naughty_word_log_message(message.author, message.content, message.guild)
-            return
-    if await RECENT_POSTS.should_ban(message):
-        reason = f'Recent posts have been deemed banworthy by our robot overlords'
+    mod_action, reason = await RECENT_POSTS.get_mod_action(message)
+    if mod_action == ModAction.NOTHING:
+        return
+    await send_log_message(message, mod_action, reason)
+    if mod_action == ModAction.WARN:
+        await message.reply(reason)
+    if mod_action == ModAction.BAN:
         await message.author.ban(reason=reason, delete_message_seconds=120)
-        await send_rate_limit_log_message(message.author, message.guild)
 
 
-async def send_naughty_word_log_message(author: User | Member, content: str, guild: Guild):
-    escaped_message = content.replace('```', '` ` `')
-    full_message = f'Banning `{author.name}` (<@{author.id}>) for message:\n```\n{escaped_message}\n```'
-    await send_log_message(guild, full_message)
+def attachments_to_files(attachments: list[discord.Attachment]) -> list[discord.File]:
+    files = []
+    for attachment in attachments:
+        response = requests.get(attachment.url)
+        if response.status_code != 200:
+            continue
+        content = BytesIO(response.content)
+        files.append(discord.File(content, attachment.filename))
+    return files
 
 
-async def send_rate_limit_log_message(author: User | Member, guild: Guild):
-    message = f'Banning `{author.name}` (<@{author.id}>) because recent posts have been deemed banworthy by our robot overlords'
-    await send_log_message(guild, message)
-
-
-async def send_sus_delete_log_message(author: User | Member, guild: Guild, description: str):
-    message = f'Deleting sus message by `{author.name}` (<@{author.id}>):\n' + description
-    await send_log_message(guild, message)
-
-
-async def send_log_message(guild: Guild, message: str):
-    for channel in guild.text_channels:
+async def send_log_message(message: discord.Message, mod_action: ModAction, reason: str):
+    escaped_message = message.content.replace('```', '` ` `')
+    verb = 'Banning' if mod_action == ModAction.BAN else 'Warning'
+    log_msg = f'{verb} `{message.author.name}` (<@{message.author.id}>):\n{reason}\n\n```\n{escaped_message}\n```'
+    files = attachments_to_files(message.attachments)
+    print(f'[{message.created_at}] {message.guild.name=} {log_msg}')
+    for channel in message.guild.text_channels:
         if channel.name in ('actual-log', 'alerta'):
-            await channel.send(message)
-
+            await channel.send(log_msg, files=files, embeds=message.embeds)
 
 
 def main():
