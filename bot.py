b9257b10dea7fe0748e9aad1eb246bf59bc4c2fb
HSZemi <hszemi@users.noreply.github.com>
2026-06-19 14:22:56 +0200

    Link recent messages by the author in warn log messages

--- bot.py
+++ bot.py
@@ -34,6 +34,7 @@ client = discord.Client(intents=intents)
 @dataclass
 class ChannelPost:
     channel_id: int
+    url: str
     author_id: int
     timestamp: float
     media_tuple: tuple[int, int, int] | None
@@ -65,7 +66,7 @@ class RecentPosts:
         self.recent_posts = []
         self.lock = asyncio.Lock()
 
-    async def get_mod_action(self, message: discord.Message) -> tuple[ModAction, str]:
+    async def get_mod_action(self, message: discord.Message) -> tuple[ModAction, str, list[str]]:
         async with self.lock:
             channel_id = message.channel.id
             author_id = message.author.id
@@ -78,32 +79,34 @@ class RecentPosts:
             contains_bad_word = any(w in message.content for w in BAD_WORDS)
             self.recent_posts.append(ChannelPost(
                 channel_id=channel_id,
+                url=message.jump_url,
                 author_id=author_id,
                 timestamp=now,
                 media_tuple=media_tuple,
                 contains_bad_word=contains_bad_word,
             ))
             posts_by_author = [p for p in self.recent_posts if p.author_id == author_id]
+            posts_urls = [p.url for p in posts_by_author]
             bad_words_count = len({p for p in posts_by_author if p.contains_bad_word})
             same_media_tuple_count = len(
                 [p for p in posts_by_author if p.media_tuple == media_tuple]) if media_tuple else 0
             recent_channels_count = len({p.channel_id for p in posts_by_author})
 
             if same_media_tuple_count >= MEDIA_BAN_THRESHOLD_INCL:
-                return ModAction.BAN, f"Same media types in {same_media_tuple_count}/{MEDIA_BAN_THRESHOLD_INCL} messages within {SECONDS_THRESHOLD} seconds"
+                return ModAction.BAN, f"Same media types in {same_media_tuple_count}/{MEDIA_BAN_THRESHOLD_INCL} messages within {SECONDS_THRESHOLD} seconds", posts_urls
             if recent_channels_count >= MSG_BAN_THRESHOLD_INCL:
-                return ModAction.BAN, f"Posted in {recent_channels_count}/{MSG_BAN_THRESHOLD_INCL} channels within {SECONDS_THRESHOLD} seconds"
+                return ModAction.BAN, f"Posted in {recent_channels_count}/{MSG_BAN_THRESHOLD_INCL} channels within {SECONDS_THRESHOLD} seconds", posts_urls
             if bad_words_count >= BAD_WORD_BAN_THRESHOLD_INCL:
-                return ModAction.BAN, f"Posted {bad_words_count}/{BAD_WORD_BAN_THRESHOLD_INCL} bad words within {SECONDS_THRESHOLD} seconds"
+                return ModAction.BAN, f"Posted {bad_words_count}/{BAD_WORD_BAN_THRESHOLD_INCL} bad words within {SECONDS_THRESHOLD} seconds", posts_urls
 
             if same_media_tuple_count >= MEDIA_WARN_THRESHOLD_INCL:
-                return ModAction.WARN, "Slow down with your posting or you will get banned"
+                return ModAction.WARN, "Slow down with your posting or you will get banned", posts_urls
             if recent_channels_count >= MSG_WARM_THRESHOLD_INCL:
-                return ModAction.WARN, "Slow down with your posting or you will get banned"
+                return ModAction.WARN, "Slow down with your posting or you will get banned", posts_urls
             if bad_words_count >= BAD_WORD_WARN_THRESHOLD_INCL:
-                return ModAction.WARN, "Do not try to tag this many people you silly goose. It does not work, and you will get banned if you try again."
+                return ModAction.WARN, "Do not try to tag this many people you silly goose. It does not work, and you will get banned if you try again.", posts_urls
 
-            return ModAction.NOTHING, ""
+            return ModAction.NOTHING, "", []
 
 
 RECENT_POSTS = RecentPosts()
@@ -120,10 +123,10 @@ async def on_ready():
 async def on_message(message: discord.Message):
     if message.author.bot:
         return
-    mod_action, reason = await RECENT_POSTS.get_mod_action(message)
+    mod_action, reason, urls = await RECENT_POSTS.get_mod_action(message)
     if mod_action == ModAction.NOTHING:
         return
-    await send_log_message(message, mod_action, reason)
+    await send_log_message(message, mod_action, reason, urls)
     if mod_action == ModAction.WARN:
         await message.reply(reason)
     if mod_action == ModAction.BAN:
@@ -141,10 +144,11 @@ def attachments_to_files(attachments: list[discord.Attachment]) -> list[discord.
     return files
 
 
-async def send_log_message(message: discord.Message, mod_action: ModAction, reason: str):
+async def send_log_message(message: discord.Message, mod_action: ModAction, reason: str, urls: list[str]):
     escaped_message = message.content.replace('```', '` ` `')
     verb = 'Banning' if mod_action == ModAction.BAN else 'Warning'
-    log_msg = f'{verb} `{message.author.name}` (<@{message.author.id}>):\n{reason}\n\n```\n{escaped_message}\n```'
+    recent_messages = "" if mod_action == ModAction.BAN else "Recent messages:\n" + "\n".join(urls)
+    log_msg = f'{verb} `{message.author.name}` (<@{message.author.id}>):\n{reason}\n{recent_messages}\n```\n{escaped_message}\n```'
     files = attachments_to_files(message.attachments)
     print(f'[{message.created_at}] {message.guild.name=} {log_msg}')
     for channel in message.guild.text_channels:
